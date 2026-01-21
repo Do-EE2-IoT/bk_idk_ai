e95eebddad61e6a87e88fe2140ae7a4ce568ce3d
Do-EE2-IoT <do30032003.com>
2026-01-21 17:27:16 +0700

    chore: check i2c

--- middleware/driver/i2c/sim_i2c_driver.c
+++ middleware/driver/i2c/sim_i2c_driver.c
@@ -550,7 +550,8 @@ bk_err_t bk_i2c_init(i2c_id_t id, const i2c_config_t *cfg)
 	bk_gpio_pull_down(HWD_GPIO_I2C_SCL);
 	gpio_dev_unmap(HWD_GPIO_I2C_SDA);
 	gpio_dev_unmap(HWD_GPIO_I2C_SCL);
-	I2cInit();
+	os_printf("Use i2c sda = %d & scl %d \r\n", HWD_GPIO_I2C_SDA, HWD_GPIO_I2C_SCL);
+		I2cInit();
 
 	return BK_OK;
 }
