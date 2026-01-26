7f0fed46bedc2d207a6c2e9244c0fa021c6ff802
Do-EE2-IoT <do30032003.com>
2026-01-26 15:53:59 +0700

    chore: add soem log

--- components/bk_gsensor/gsensor_sc7a20.c
+++ components/bk_gsensor/gsensor_sc7a20.c
@@ -213,7 +213,7 @@ static void gsensor_sc7a20_event_handler(gsensor_mode_t t_runmode)
             dat->xyz[i].x = (short)((sc7a20_data[1] << 8) | sc7a20_data[0]);
             dat->xyz[i].y = (short)((sc7a20_data[3] << 8) | sc7a20_data[2]);
             dat->xyz[i].z = (short)((sc7a20_data[5] << 8) | sc7a20_data[4]);
-            os_printf("xyz:%d,%d,%d\r\n",dat->xyz[i].x,dat->xyz[i].y,dat->xyz[i].z);
+            os_printf("xyz:%d,%d,%d\r\n", dat->xyz[i].x, dat->xyz[i].y, dat->xyz[i].z);
         }
         if (datacb)
             datacb((void *)&gs_sc7a20, dat);
@@ -234,6 +234,7 @@ static void gsensor_sc7a20_thread(beken_thread_arg_t arg)
 
     while (1)
     {
+        os_printf("ret = rtos_get_semaphore(&s_gsensor_sc7a20_event_wait, BEKEN_WAIT_FOREVER); \r\n");
         ret = rtos_get_semaphore(&s_gsensor_sc7a20_event_wait, BEKEN_WAIT_FOREVER);
         if (kNoErr == ret)
         {
